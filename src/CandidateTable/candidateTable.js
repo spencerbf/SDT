ca3bf0f953c21e87ac154debf45b132f95315a07
spencer barton-fisher <spencer.bartonfisher@me.com>
2020-02-23 21:10:24 +1100

    initial commit, created project set up

---
+++ src/CandidateTable/candidateTable.js
@@ -0,0 +1,155 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableSortLabel from "@material-ui/core/TableSortLabel";
+import Paper from "@material-ui/core/Paper";
+import axios from "axios";
+
+function createData(name, exp, favLang, ausCit, protein) {
+  return { name, exp, favLang, ausCit, protein };
+}
+
+const rows = [
+  createData("Dave", "3", "ruby", "yes", "cat"),
+  createData("Jeff", "5", "js", "no", "dog"),
+  createData("George", "7", "python", "yes", "mouse")
+];
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === "desc"
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
+
+const headCells = [
+  {
+    id: "name",
+    numeric: false,
+    disablePadding: true,
+    label: "Full Name"
+  },
+  { id: "exp", numeric: true, disablePadding: false, label: "exp" },
+  {
+    id: "favLang",
+    numeric: false,
+    disablePadding: false,
+    label: "Fav Language"
+  },
+  {
+    id: "ausCit",
+    numeric: false,
+    disablePadding: false,
+    label: "Australian Citizen"
+  },
+  { id: "protein", numeric: false, disablePadding: false, label: "Protein (g)" }
+];
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%"
+  },
+  paper: {
+    width: "100%",
+    marginBottom: theme.spacing(2)
+  },
+  table: {
+    minWidth: 750
+  }
+}));
+
+export const EnhancedTable = () => {
+  const classes = useStyles();
+  const [order, setOrder] = useState("asc");
+  const [orderBy, setOrderBy] = useState("name");
+  const [candidates, setCandidates] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("./candidateData.json");
+      setCandidates(result.data);
+    };
+    fetchData();
+  }, []);
+
+  console.log(candidates);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const createSortHandler = property => event => {
+    handleRequestSort(event, property);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Paper>
+        <TableContainer>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                {headCells.map(headCell => (
+                  <TableCell
+                    key={headCell.id}
+                    sortDirection={orderBy === headCell.id ? order : false}
+                  >
+                    <TableSortLabel
+                      direction={orderBy === headCell.id ? order : "asc"}
+                      onClick={createSortHandler(headCell.id)}
+                    >
+                      {headCell.label}
+                    </TableSortLabel>
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {stableSort(rows, getComparator(order, orderBy)).map(row => {
+                return (
+                  <TableRow hover tabIndex={-1} key={row.name}>
+                    <TableCell scope="row" padding="none">
+                      {row.name}
+                    </TableCell>
+                    <TableCell>{row.exp}</TableCell>
+                    <TableCell>{row.favLang}</TableCell>
+                    <TableCell>{row.ausCit}</TableCell>
+                    <TableCell>{row.protein}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    </div>
+  );
+};
+
+export default EnhancedTable;
